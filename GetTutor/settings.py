85aa79d1a8ad05c73896fb96803b40ab3c908a77
Mide Dickson <sosanyaolumidedoubled@gmail.com>
2021-04-25 12:51:44 +0100

    heroku settings

--- GetTutor/settings.py
+++ GetTutor/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,5 +149,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CORS_ORIGIN_ALLOW_ALL = True
-import django_heroku
 django_heroku.settings(locals())
